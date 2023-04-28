60367d465b638bb2471f764c2a5e78b6db19e0f5
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-04-28 14:50:42 +0200

    add swagger to handle api documentation

---
+++ ts-server-functional/src/API-docs/swagger.ts
@@ -0,0 +1,13 @@
+export const swaggerDocument = {
+	openapi: '3.0.1',
+	info: {
+		version: '1.0.0',
+		title: 'API Documentation',
+		description: 'The description here',
+		contact: {
+      name: 'Felipe Leite Mantovani',
+      email: 'felipe.mantovani@outlook.com',
+      github: 'https://github.com/f-mantovani'
+  }
+	},
+}
--- ts-server-functional/src/app.ts
+++ ts-server-functional/src/app.ts
@@ -5,17 +5,23 @@ import log, { requestLogger } from './utils/logger'
 import { routes } from './routes'
 import { connectDB } from './utils/connectDB'
 import { errorHandling } from './routes/errorHandling'
+import { swaggerDocument } from './API-docs/swagger'
+import swaggerUi from 'swagger-ui-express'
 
 export const app: Express = express()
 
 export async function main() {
-	log.info(`Server connected to ${process.env.PORT || 8080}`)
+	log.info(`Server running on http://localhost:${process.env.PORT || 8080}`)
 
 	await connectDB()
 	
 	app.use(express.json())
 
+	app.use(cors())
+
 	app.use(requestLogger)
+
+	app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 	
 	app.use('/api', routes())
 
